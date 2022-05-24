f262daf79ae74d7ad951f74b9d3324d50cc77f80
Antoine Pain <apain@biac.lu>
2022-05-24 22:32:58 +0200

    Init project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-navigation-drawer permanent>
+      <v-list nav>
+        <v-list-item
+          prepend-icon="mdi-account-group-outline"
+          title="Teams"
+          value="teams"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-podium"
+          title="Ranking"
+          value="ranking"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-sword-cross"
+          title="Round"
+          value="round"
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar color="primary"></v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts"></script>
